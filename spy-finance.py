53d449bd5e1483daf72a39ea6693b854693e0ca7
jacklin92 <jacklin92xxx@gmail.com>
2024-06-18 11:37:14 +0800

    Add the function of identifying whether the trend is up/down

--- spy-finance.py
+++ spy-finance.py
@@ -15,20 +15,15 @@ for i in range(len(stock_num)):
     soup = BeautifulSoup(web.text, "html.parser")  # 轉換內容
     title = soup.find("h1")  # 找到 h1 的內容
     a = soup.select(".Fz\(32px\)")[0]  # 找到第一個 class 為 Fz(32px) 的內容，如果出現錯誤，可以使用 .Fz\(32px\) 轉義
-    b = soup.select(".Fz\(20px\)")[0]  # 找到第一個 class 為 Fz(20px) 的內容，如果出現錯誤，可以使用 .Fz\(20px\) 轉義
+    b = soup.select(".Fz\(20px\)")[1]  # 找到第一個 class 為 Fz(20px) 的內容，如果出現錯誤，可以使用 .Fz\(20px\) 轉義
     s = ""  # 漲或跌的狀態
     try:
-    # 如果 main-0-QuoteHeader-Proxy id 的 div 裡有 C($c-trend-down) 的 class
-    # 表示狀態為下跌
-        if soup.select("#main-0-QuoteHeader-Proxy")[0].select(".C($c-trend-down)")[0]:
-            s = "-"
+    # 如果 main-0-QuoteHeader-Proxy id 的 div 裡有 C($c-trend-down) 的 class表示狀態為下跌，若是C($c-trend-up)則是上漲
+        if soup.select("#main-0-QuoteHeader-Proxy")[0].select(".C\\(\\$c-trend-up\\)"):
+            s = "+"
+        elif soup.select("#main-0-QuoteHeader-Proxy")[0].select(".C\\(\\$c-trend-down\\)"):
+            s="-"
+        print(f"{title.get_text()} {stock_num[i]}: {a.get_text()} ( {s}{b.get_text()} )")  # 印出結果
     except:
-        try:
-        # 如果 main-0-QuoteHeader-Proxy id 的 div 裡有 C($c-trend-up) 的 class
-        # 表示狀態為上漲
-            if soup.select("#main-0-QuoteHeader-Proxy")[0].select(".C($c-trend-up)")[0]:
-                s = "+"
-        except:
-        # 如果都沒有包含，表示平盤
-            s = "-"
-    print(f"{title.get_text()} {stock_num[i]}: {a.get_text()} ( {s}{b.get_text()} )")  # 印出結果
+            s = "Error"
+            print(s)  # 印出結果
